310902028d13b966a837d0780523b3fb9dfac160
IvoBenitez <estudiosivo@gmail.com>
2022-12-29 15:48:52 -0300

    probando

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+const publicPatch = path.resolve(__dirname, './public');
+app.use( express.static(publicPatch) );
+
+const port= process.env.PORT || 3001
+
+    app.listen(port, ()=>console.log("Servidor corriendo en el puerto http://localhost:" + port));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/MercadoLiebre.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
